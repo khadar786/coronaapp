467617b1eaea37ebe55e2dbdba97d6dabf30027b
S Khadar Basha <khadarbasha@hgtlkkd.com>
2024-08-31 20:22:31 +0530

    add app command

--- app.py
+++ app.py
@@ -103,4 +103,6 @@ def update_graph(type):
                 'layout':go.Layout(title='State Total Count')}
 
 if __name__=='__main__':
-    app.run('0.0.0.0',port='5000')
+    app.run_server(debug=True)
+
+server = app.server  # Expose server for Gunicorn
